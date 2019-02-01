85a41ceda618ca038f5a60836479f66b3609e4ea
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2019-02-01 17:27:21 +0100

    base config with xmobar

---
+++ xmonad.hs
@@ -0,0 +1,22 @@
+import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys)
+import System.IO
+
+main = do
+    xmproc <- spawnPipe "/usr/sbin/xmobar /home/akkerman/.xmobarrc"
+
+    xmonad $ docks $ defaultConfig
+        { manageHook = manageDocks <+> manageHook defaultConfig
+        , layoutHook = avoidStruts  $  layoutHook defaultConfig
+        , logHook = dynamicLogWithPP xmobarPP
+                        { ppOutput = hPutStrLn xmproc
+                        , ppTitle = xmobarColor "green" "" . shorten 50
+                        }
+        , modMask = mod4Mask     -- Rebind Mod to the Windows key
+        } `additionalKeys`
+        [ ((mod4Mask .|. mod1Mask, xK_l), spawn "slock")
+        ]
+

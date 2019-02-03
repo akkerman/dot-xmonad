449cd792b2f17eb1dbf2342e558d50823c07fb4b
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2019-02-03 16:10:30 +0100

    Add three column layout

--- xmonad.hs
+++ xmonad.hs
@@ -4,13 +4,15 @@ import XMonad.Hooks.ManageDocks
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
+import XMonad.Layout.ThreeColumns
+
 
 main = do
     xmproc <- spawnPipe "/usr/sbin/xmobar /home/akkerman/.xmobarrc"
 
     xmonad $ docks $ defaultConfig
         { manageHook = manageDocks <+> manageHook defaultConfig
-        , layoutHook = avoidStruts  $  layoutHook defaultConfig
+        , layoutHook = myLayout
         , logHook = dynamicLogWithPP xmobarPP
                         { ppOutput = hPutStrLn xmproc
                         , ppTitle = xmobarColor "green" "" . shorten 50
@@ -20,3 +22,11 @@ main = do
         [ ((mod4Mask .|. mod1Mask, xK_l), spawn "slock")
         ]
 
+myLayout =
+    avoidStruts $ tiled ||| three ||| Full 
+    where 
+        three = ThreeColMid nmaster delta (5/12)
+        tiled = Tall nmaster delta ratio
+        nmaster = 1
+        ratio = 2/3
+        delta = 3/100

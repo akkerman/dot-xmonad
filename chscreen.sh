b07e1e074364bc9b4d3803e63983b65ec0ae335f
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2019-02-04 21:17:16 +0100

    Add dmenu command to change screen layout

---
+++ chscreen.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+choice=$( find "$HOME/.screenlayout" \( -type f -o -type l \) -printf '%f\n' | sort | dmenu -i)
+
+if [ -n "$choice" ]; then
+  sh "$HOME/.screenlayout/$choice"
+  xmonad --restart
+  "$HOME/.config/polybar/launch.sh"
+
+
+  killall synergys
+  killall synergy 
+
+  while pgrep -u $UID -x synergy >/dev/null; do sleep 1; done
+  synergy &
+fi

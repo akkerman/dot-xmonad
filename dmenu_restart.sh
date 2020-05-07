30747fb067cfbc1a181152f11b0617b0c5f0557e
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2020-05-07 14:55:42 +0200

    Add wallpaper to restart script

--- dmenu_restart.sh
+++ dmenu_restart.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-choice=$(echo -e "restart status bar\nenable transparancy\ndisable transparancy" | dmenu -i "$@")
+choice=$(echo -e "restart status bar\nenable transparancy\ndisable transparancy\nreload wallpaper" | dmenu -i "$@")
 
 [ "$choice" = "restart status bar" ] && $HOME/.config/polybar/launch.sh
 [ "$choice" = "enable transparancy" ] && $HOME/.config/compton/launch.sh
 [ "$choice" = "disable transparancy" ] && killall compton
+[ "$choice" = "reload wallpaper" ] && sh $HOME/.fehbg
 

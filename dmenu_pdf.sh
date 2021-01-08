0ffe040453ffdb4131456b4c6c31a3a222ebd413
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2021-01-08 22:07:43 +0100

    Better dmenu pdf

--- dmenu_pdf.sh
+++ dmenu_pdf.sh
@@ -1,7 +1,27 @@
 #!/bin/sh
-choice=$(find "$HOME/Dropbox/Apps" "$HOME/Dropbox/Library Bought" "$HOME/Dropbox/Library" "$HOME/Documents" -name "*.pdf"  -o -name "*.epub" | dmenu -l 20 -i "$@")
+choice=$(find "$HOME/Dropbox/Apps" "$HOME/Dropbox/Library Bought" "$HOME/Dropbox/Library" "$HOME/Documents" "$HOME/git/dsplatform/analytics-wiki" -name "*.pdf"  -o -name "*.epub" -o -name "*.mobi" -o -name "*.md" | dmenu -l 20 -i "$@")
+
+msg="opened nothing"
+
+
 
 if [ -n "$choice" ]; then
-  zathura "$choice"
+  filename=$(basename -- "$choice")
+  extension="${filename##*.}"
+  filename="${filename%.*}"
+  case $extension in
+    "pdf")
+      exec zathura "$choice"
+      ;;
+    "mobi")
+      exec ebook-viewer "$choice"
+      ;;
+    "epub")
+      exec ebook-viewer "$choice"
+      ;;
+    "md")
+      exec st -e nvim "$choice" +MarkdownPreview
+      notify-send "opening $choice"
+      ;;
+  esac
 fi
-

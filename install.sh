dcf6dc0c9f28c487b3c3d4a73a26d7a508cd6a56
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2022-01-04 21:42:34 +0100

    Use xmonad 17
    switch from arch packages to cabal

---
+++ install.sh
@@ -0,0 +1,2 @@
+cabal install --package-env=$HOME/.config/xmonad --lib xmonad xmonad-contrib xmonad-dbus dbus utf8-string
+cabal install --package-env=$HOME/.config/xmonad xmonad --overwrite-policy=always

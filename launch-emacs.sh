d79d90c9c4db8d30d803d2826bc86ffca8c25d2f
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2022-04-04 10:35:42 +0200

    Better emacs launch script

--- launch-emacs.sh
+++ launch-emacs.sh
@@ -1,2 +1,12 @@
 #!/usr/bin/env bash
-/usr/bin/emacsclient -e "(kill-emacs)" && /usr/bin/emacs --daemon &
+
+# Gracefully shutdown emacs if it is running
+pgrep -u $UID -x emacs >/dev/null && /usr/bin/emacsclient -e "(kill-emacs)" 
+
+# Wait until emacs is shutdown
+while pgrep -u $UID -x emacs >/dev/null; do sleep 1; done
+
+# Startup 
+/usr/bin/emacs --daemon &
+
+notify-send "Emacs started"

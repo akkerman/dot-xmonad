1f8317cdf4d3ec05b39f3a5b7796ebe00347963a
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2022-04-09 09:03:36 +0200

    Add shortcut to qutebrowser

--- lib/ShortCuts.hs
+++ lib/ShortCuts.hs
@@ -68,6 +68,7 @@ modify conf = conf
 
     -- programs
     , ("M-g"                        , spawn "chromium --profile-directory=Default")
+    , ("M-q"                        , spawn "qutebrowser")
     , ("M-S-f"                      , spawn "firefox")
     , ("M-S-g"                      , spawn "chromium --incognito")
     , ("M-y"                        , spawn "chromium --profile-directory=Default --app-id=agimnkijcaahngcdmfeangaknmldooml") -- youtube

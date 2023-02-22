1b9b5c2ca64151e02bf8b3d76a16315a58324854
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2023-02-22 12:42:39 +0100

    Do not restart synergy

--- chscreen.sh
+++ chscreen.sh
@@ -8,9 +8,9 @@ if [ -n "$choice" ]; then
   "$HOME/.config/polybar/launch.sh"
 
 
-  killall synergys
-  killall synergy 
+  # killall synergys
+  # killall synergy 
 
-  while pgrep -u $UID -x synergy >/dev/null; do sleep 1; done
-  synergy &
+  # while pgrep -u $UID -x synergy >/dev/null; do sleep 1; done
+  # synergy &
 fi

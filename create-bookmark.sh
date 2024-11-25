adfa40f2424a97f9f3d0968880d4ee9b5c48d560
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2024-11-25 09:16:36 +0100

    Check if added book mark is an url

--- create-bookmark.sh
+++ create-bookmark.sh
@@ -3,12 +3,17 @@
 bookmark="$(xclip -o)"
 file="$HOME/Dropbox/bookmarks.txt"
 
+if ! [[ $bookmark =~ ^(https?|ftp|file|chrome)://[a-zA-Z0-9./?=_%+-]+$ ]]; then
+  notify-send -t 3000 -u critical "Invalid URL" "$bookmark"
+  exit 1
+fi
 
 if grep -q "^$bookmark$" "$file"; then
   notify-send -t 3000 -u critical "Bookmark exists"
-else
-  echo "$bookmark" >> $file
-  cat .config/qutebrowser/bookmarks/urls $file | sort | uniq > .config/qutebrowser/bookmarks/urls
-
-  notify-send -t 2500 "Bookmark added" "$bookmark"
+  exit 1
 fi
+
+echo "$bookmark" >> $file
+cat ~/.config/qutebrowser/bookmarks/urls $file | sort | uniq | grep -E 'http|file' > ~/.config/qutebrowser/bookmarks/urls
+
+notify-send -t 2500 "Bookmark added" "$bookmark"

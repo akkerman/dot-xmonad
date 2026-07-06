a96dd732c1c9160e605fcd3d52cf35a4170d734e
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2026-07-06 09:48:34 +0200

    Extract URL from bookmark choice by taking part before first space

--- insert-bookmark.sh
+++ insert-bookmark.sh
@@ -3,5 +3,5 @@
 choice=$(grep -v '^#' $HOME/Dropbox/bookmarks.txt | sort | dmenu -p "Insert bookmark" -i -l 20 "$@")
 
 if [ -n "$choice" ]; then
-  xdotool type "$choice"
+  xdotool type "${choice%% *}"
 fi
--- launch-bookmark.sh
+++ launch-bookmark.sh
@@ -3,5 +3,5 @@
 choice=$(grep -v '^#' $HOME/Dropbox/bookmarks.txt | sort | dmenu -p "Open bookmark" -i -l 20 "$@")
 
 if [ -n "$choice" ]; then
- qutebrowser --target window "$choice"
+ qutebrowser --target window "${choice%% *}"
 fi
